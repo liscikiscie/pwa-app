ad8713ab3e455d213439253f0f78f42ef18939ef
notFromHere <jakubwasiljew@gmail.com>
2019-01-09 12:01:15 +0100

    fetch & promise for older browser (polyfills)

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,15 +1,22 @@
 let deferredPrompt;
 
+if ( !window.Promise ) {
+    window.Promise = Promise;
+}
+
 if ( 'serviceWorker' in navigator ) {
     navigator
         .serviceWorker
         .register('/sw.js', {})
         .then(function () {
             console.log('Service worker registered');
-        });
+        })
+        .catch(function ( error ) {
+            console.log(error);
+        })
 }
 
-window.addEventListener('beforeinstallprompt', function (event) {
+window.addEventListener('beforeinstallprompt', function ( event ) {
     console.log('beforeinstallprompt fired');
     event.preventDefault();
     deferredPrompt = event;
